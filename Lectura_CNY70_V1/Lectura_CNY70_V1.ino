7ec610a3bd2a53f654a1b08c8bdcd8ff8b5366f2
RodrigoSturm14 <rodristurm@gmail.com>
2023-07-21 12:13:48 -0300

    Agregue pines

--- Lectura_CNY70_V1/Lectura_CNY70_V1.ino
+++ Lectura_CNY70_V1/Lectura_CNY70_V1.ino
@@ -1,14 +1,21 @@
-int PinLectura = A0;
+#define PIN_CNY1 4
+#define PIN_CNY2 2
 
 void setup() {
-  
-  pinMode(PinLectura, INPUT);
-  Serial.begin(9600);
+  pinMode(PIN_CNY1, INPUT);
+  pinMode(PIN_CNY2, INPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
+  int CNY1 = analogRead(PIN_CNY1);
+  int CNY2 = analogRead(PIN_CNY2);
 
-  Serial.println("Lectura: ");
-  Serial.println(analogRead(PinLectura));
-  delay(2000);
+  Serial.print("Lectura CNY1: ");
+  Serial.println(CNY1);
+
+  Serial.print("Lectura CNY2: ");
+  Serial.println(CNY2);
+
+  delay(1500);
 }
